baee2bbf3d7fde41eed9f06a5900a9de59b9123a
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-03-20 15:36:34 +0200

    Add files via upload

---
+++ Laborator1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+    using System;
+    public class PrimeNumberExample
+    {
+        public static void Main(string[] args)
+        {
+            int n, i,j, m = 0, flag = 0;
+            Console.Write("Enter a Number : ");
+            n = int.Parse(Console.ReadLine());
+ 
+            for (i = n-1; i > 0; i--)
+            {
+            int divizori = 0;
+                for (j = 2; j<=n; j++)
+                {
+                    if(i%j==0)
+                    {
+                    divizori++;
+                    
+                    }
+                }
+
+            if (divizori < 2)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+        }
+
+            
+            
+        }
+    }
+
