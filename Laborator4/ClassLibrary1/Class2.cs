10f3f9d93c332ccff7549bdaacaa4660df9dfa2f
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-03-22 11:15:07 +0200

    Add files via upload

---
+++ Laborator4/ClassLibrary1/Class2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+
+    public class Class2 : DbContext
+    {
+        public DbSet<Class1> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("DESKTOP-NF1T29D");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Class1>();
+        }
+    }
+
+
+}
