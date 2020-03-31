367ecfbf3b5906ae4e2c650725b039d96d5f37fa
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-03-31 22:34:51 +0300

    Add files via upload

---
+++ Laborator7/ObjectWCF/InterfaceWCF.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using PostComment;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+
+namespace ObjectWCF
+{
+    [ServiceContract] interface InterfacePost
+    {
+
+        [OperationContract] bool AddPost(Post post);
+
+        [OperationContract] Post GetPostById(int id);
+
+        [OperationContract] int DeletePost(int id);
+        
+        [OperationContract] List<Post> GetPosts();
+
+        [OperationContract] Post UpdatePost(Post post);
+
+    }
+
+    [ServiceContract] interface InterfaceComment
+    {
+
+        [OperationContract] bool AddComment(Comment comment);
+
+        [OperationContract] Comment UpdateComment(Comment newComment);
+
+        [OperationContract] Comment GetCommentById(int id);
+
+    }
+
+    [ServiceContract] interface IPostComment: InterfacePost, InterfaceComment{}
+
+    class InterfaceWCF { }
+      
+}
