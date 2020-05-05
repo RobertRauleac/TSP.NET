7abdc11c615f44f6cbd6d7ca5781ce2f9df270d9
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-05-05 14:09:10 +0300

    Add files via upload

---
+++ Asp_Rauleac_Robert_Rp/Data/Asp_Rauleac_Robert_RpContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Asp_Rauleac_Robert_Rp.Models
+{
+    public class Asp_Rauleac_Robert_RpContext : DbContext
+    {
+        public Asp_Rauleac_Robert_RpContext(DbContextOptions<Asp_Rauleac_Robert_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Rauleac_Robert_Rp.Models.Movie> Movie { get; set; }
+    }
+}
