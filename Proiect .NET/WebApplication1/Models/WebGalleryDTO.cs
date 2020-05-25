66046b400ee41451719a224280e44580c17e8e58
RobertRauleac <48402944+RobertRauleac@users.noreply.github.com>
2020-05-26 00:27:34 +0300

    Add files via upload

---
+++ Proiect .NET/WebApplication1/Models/WebGalleryDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceReference1;
+using System.Runtime.Serialization;
+
+namespace WebApplication1.Models
+{
+    [DataContract(IsReference = true)]
+    public class WebGalleryDTO
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Size { get; set; } 
+
+        [DataMember]
+        public string Path { get; set; }
+
+        [DataMember]
+        public string Format { get; set; }
+
+        [DataMember]
+        public string Date { get; set; }
+    }
+}
